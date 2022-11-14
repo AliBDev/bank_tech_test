7e45b77e42e2cded6df86d011ea587012349cd88
Ali <abhatti89@gmail.com>
2022-11-14 17:55:57 +0000

    Deposit test

--- spec/account.test.js
+++ spec/account.test.js
@@ -10,4 +10,11 @@ describe('Account', () => {
   test('Should start with an initial balance of 0', () => {
     expect(account.balance).toEqual(0)
   });
+
+  test('Should show a balance of 100 after 100 has been deposited', () => {
+    account.add(100);
+
+    expect(account.balance).toEqual(100)
+  });
+  
 })
