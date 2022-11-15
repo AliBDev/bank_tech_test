b28d3ca3ae6b961e2934516f2fdca3d3da47920f
Ali <abhatti89@gmail.com>
2022-11-15 17:47:28 +0000

    Tests for adding a deposit & updating the balance

--- spec/statement.test.js
+++ spec/statement.test.js
@@ -13,4 +13,24 @@ describe('Statement', () => {
   it('returns a statement with no transactions', () => {
     expect(statement.getStatement()).toEqual([]);
   })
+
+  it("Adds a deposit to the statement", () => {
+    const deposit = { amount: 1500, type: "deposit" };
+    statement.add(deposit);
+
+    const statementDetails = statement.getStatement();
+    expect(statementDetails[0].includes("1500.00")).toBe(true);
+  });
+
+  it("Adds a deposit and balance is updated", () => {
+    const account = new Account();
+    account.deposit(2000);
+    const statement = new Statement(account);
+
+    const deposit = { amount: 2000, type: "deposit" };
+    statement.add(deposit);
+
+    const statementDetails = statement.getStatement();
+    expect(statementDetails[0]).toEqual("15/11/2022 || 2000.00 || || 2000.00")
+  });
 });
