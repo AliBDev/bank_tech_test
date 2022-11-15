72ed016b6e7a2fa50f98879d2f5abff674a5ac6d
Ali <abhatti89@gmail.com>
2022-11-15 13:04:26 +0000

    Formatter class test for converting into currrency format

--- spec/formatter.test.js
+++ spec/formatter.test.js
@@ -1,10 +1,17 @@
 const Formatter = require('../lib/formatter.js');
 
 describe('Formatter', () => {
-  formatter = new Formatter
+ const formatter = new Formatter()
 
   test("Return today's date", () => {
     expect(formatter.todaysDate()).toEqual("15/11/2022")
   });
-  
+})
+
+describe('Formatter', () => {
+ const formatter = new Formatter()
+
+  test('Return the amount formatted with 2 decimal places: 100.00', () => {  
+    expect(formatter.convertAmountToCurrency(100)).toEqual("100.00")
+  });
 })
