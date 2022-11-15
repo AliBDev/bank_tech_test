b3e96557c05d15b1e266c3ef35032217fed88b8a
Ali <abhatti89@gmail.com>
2022-11-15 14:33:14 +0000

    Columns formatting tests passing

--- spec/formatter.test.js
+++ spec/formatter.test.js
@@ -1,17 +1,28 @@
 const Formatter = require('../lib/formatter.js');
+const formatter = new Formatter();
 
-describe('Formatter', () => {
- const formatter = new Formatter()
-
-  test("Return today's date", () => {
-    expect(formatter.todaysDate()).toEqual("15/11/2022")
+describe('Date Formatter', () => {
+  it("Returns today's date", () => {
+    expect(formatter.todaysDate()).toEqual("15/11/2022");
   });
 })
 
-describe('Formatter', () => {
- const formatter = new Formatter()
+describe('Currency Formatter', () => {
+  it('Returns the amount formatted with 2 decimal places: 100.00', () => {  
+    expect(formatter.convertAmountToCurrency(100)).toEqual("100.00");
+  });
+})
 
-  test('Return the amount formatted with 2 decimal places: 100.00', () => {  
-    expect(formatter.convertAmountToCurrency(100)).toEqual("100.00")
+describe('Columns Formatter', () => {
+  it('Returns the formatting of a deposit', () => {
+    transaction = { amount: 500, type: "deposit" };
+    
+    expect(formatter.statementColumns(transaction)).toEqual(" 500.00 || ");
+  });
+
+  it('Returns the formatting of a withdrawal', () => {  
+    transaction = { amount: 400, type: "withdrawal" };
+    
+    expect(formatter.statementColumns(transaction)).toEqual(" || 400.00 ");
   });
 })
