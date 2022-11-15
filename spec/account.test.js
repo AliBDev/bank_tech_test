9fac7683de128b4152527cd897abeebaca2b64ab
Ali <abhatti89@gmail.com>
2022-11-15 15:02:24 +0000

    Current balance functionality added

--- spec/account.test.js
+++ spec/account.test.js
@@ -7,21 +7,27 @@ describe('Account', () => {
     account = new Account()
   })
 
-  test('Should start with an initial balance of 0', () => {
+  it('Should start with an initial balance of 0', () => {
     expect(account.balance).toEqual(0)
   });
 
-  test('Should show a balance of 100 after 100 has been deposited', () => {
-    account.add(1000);
+  it('Should show a balance of 100 after 100 has been deposited', () => {
+    account.deposit(1000);
 
     expect(account.balance).toEqual(1000)
   });
 
-  test('Should allow for withdrawals', () => {
-    account.add(1000);
+  it('Should allow for withdrawals', () => {
+    account.deposit(1000);
     account.withdrawal(500);
     
     expect(account.balance).toEqual(500)
   });
   
+  it('Should display the current balance', () => {
+    account.deposit(1000);
+    account.withdrawal(500);
+    
+    expect(account.getCurrentBalance()).toEqual("500.00")
+  });
 })
